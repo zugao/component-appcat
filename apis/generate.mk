e5fde7f914cb2fa12bcc3809b96eb3c671fef1a6
Fabian Fischer <glrf@users.noreply.github.com>
2023-01-12 09:59:51 +0100

    Improve makefile comment

--- apis/generate.mk
+++ apis/generate.mk
@@ -1,7 +1,7 @@
 clean_targets += .clean-apis
 
 .PHONY: generate-xrd
-generate-crd: ## Generates the XRDs using Kubebuilder
+generate-crd: ## Generates the CRDs using Kubebuilder, these CRDs will be used as a base for the XRDs by the component
 	@rm -rf apis/generated
 	@cd apis && go run sigs.k8s.io/controller-tools/cmd/controller-gen paths=./... crd:crdVersions=v1 output:artifacts:config=./generated
 	@cd apis && go generate ./...
