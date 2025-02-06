91b2dc1fd2e73f50dd2e716cdbaf201a33d0f9f9
Łukasz Widera <lukaszwidera1993@gmail.com>
2025-02-06 09:12:55 +0100

    cleaning up component

--- tests/e2e/forgejo/scripts/ingress.sh
+++ tests/e2e/forgejo/scripts/ingress.sh
@@ -6,5 +6,5 @@ ns=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.status
 
 fqdn=$(kubectl -n "$ns" get ingress -oyaml | yq -r '.items.[0].spec.tls.[0].hosts[0]')
 
-echo "$fqdn = somesuperingressname.apps.lab-cloudscale-rma-0.appuio.cloud"
-[[ "$fqdn" == "somesuperingressname.apps.lab-cloudscale-rma-0.appuio.cloud" ]]
+echo "$fqdn = forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud"
+[[ "$fqdn" == "forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud" ]]
