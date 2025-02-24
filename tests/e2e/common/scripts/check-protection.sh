29bd7193093bd7e864c363154cf654e28de6691c
wejdross <lukaszwidera1993@gmail.com>
2025-02-24 13:38:28 +0100

    small fixes to e2e tests

--- tests/e2e/common/scripts/check-protection.sh
+++ tests/e2e/common/scripts/check-protection.sh
@@ -7,16 +7,19 @@ name="$2"
 
 ns=$(kubectl -n "$NAMESPACE" get "$type" "$name" -oyaml | yq -r '.status.instanceNamespace')
 
-if ! kubectl -n "$NAMESPACE" delete "$type" "$name" ; then
+if ! kubectl -n "$NAMESPACE" delete --timeout=30s "$type" "$name" ; then
   echo "instance protected"
 else
   echo "instance got deleted! Please check deletion protection!"
-    false
+  exit 1
 fi
 
-if ! kubectl delete ns "$ns" ; then
+if ! kubectl delete --timeout=30s ns "$ns" ; then
   echo "namespace protected"
 else
   echo "namespace got deleted! Please check deletion protection!"
-  false
+  exit 1
 fi
+
+echo Returning with zero - Protections works well
+exit 0
