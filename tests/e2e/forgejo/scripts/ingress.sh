416b952121b7b315d4f7fcc9de05bf09f5a6edff
Gabriel Saratura <gabriel.saratura@vshn.net>
2025-03-12 17:02:31 +0100

    Fix forgejo e2e tests

--- tests/e2e/forgejo/scripts/ingress.sh
+++ tests/e2e/forgejo/scripts/ingress.sh
@@ -3,8 +3,12 @@
 set -exf
 
 ns=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.status.instanceNamespace')
+composite=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.spec.resourceRef.name')
 
-fqdn=$(kubectl -n "$ns" get ingress -oyaml | yq -r '.items.[0].spec.tls.[0].hosts[0]')
+fqdn=$(kubectl -n "$ns" get ingress "${composite}-wildcard-ingress" -oyaml | yq -r '.spec.rules.[0].host')
+fqdn_sub=$(kubectl -n "$ns" get ingress "${composite}-letsencrypt-ingress"  -oyaml | yq -r '.spec.tls.[0].hosts[0]')
 
 echo "$fqdn = forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud"
+echo "$fqdn_sub = sub1.forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud"
 [[ "$fqdn" == "forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud" ]]
+[[ "$fqdn_sub" == "sub1.forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud" ]]
