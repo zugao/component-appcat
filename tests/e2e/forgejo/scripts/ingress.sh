2d43e44f839e73ace265ee253f756cf52643e668
Simon Beck <simon.beck@vshn.ch>
2025-03-31 09:42:17 +0200

    Disable forgejo config check e2e for now
    It's currently broken and needs proper fixing.

--- tests/e2e/forgejo/scripts/ingress.sh
+++ tests/e2e/forgejo/scripts/ingress.sh
@@ -5,8 +5,8 @@ set -exf
 ns=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.status.instanceNamespace')
 composite=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.spec.resourceRef.name')
 
-fqdn=$(kubectl -n "$ns" get ingress "${composite}-wildcard-ingress" -oyaml | yq -r '.spec.rules.[0].host')
-fqdn_sub=$(kubectl -n "$ns" get ingress "${composite}-letsencrypt-ingress"  -oyaml | yq -r '.spec.tls.[0].hosts[0]')
+fqdn=$(kubectl -n "$ns" get ingress "${composite}-letsencrypt-ingress"  -oyaml | yq -r '.spec.tls.[0].hosts[0]')
+fqdn_sub=$(kubectl -n "$ns" get ingress "${composite}-letsencrypt-ingress"  -oyaml | yq -r '.spec.tls.[0].hosts[1]')
 
 echo "$fqdn = forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud"
 echo "$fqdn_sub = sub1.forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud"
