f57d2fa0f595a205d467103db00c84800bc3da78
Marco De Luca <marcodl404@gmail.com>
2025-05-15 21:39:39 +0200

    Extend nextcloud e2e to also check collabora

--- tests/e2e/nextcloud/scripts/ingress.sh
+++ tests/e2e/nextcloud/scripts/ingress.sh
@@ -4,7 +4,21 @@ set -e
 
 ns=$(kubectl -n "$NAMESPACE" get vshnnextcloud nextcloud-e2e -oyaml | yq -r '.status.instanceNamespace')
 
-fqdn=$(kubectl -n "$ns" get ingress -oyaml | yq -r '.items.[0].spec.tls.[0].hosts[0]')
+ingress_hosts=($(kubectl -n "$ns" get ingress -oyaml | yq -r '.items[].spec.tls[].hosts[]'))
 
-echo "$fqdn = nextcloud-e2e.example.com"
-[[ "$fqdn" == "nextcloud-e2e.example.com" ]]
+nextcloud_found=false
+collabora_found=false
+
+for host in "${ingress_hosts[@]}"; do
+    if [[ "$host" == "nextcloud-e2e.example.com" ]]; then
+        nextcloud_found=true
+    elif [[ "$host" == "collabora-e2e.example.com" ]]; then
+        collabora_found=true
+    fi
+done
+
+echo "Found ingress hosts: ${ingress_hosts[*]}"
+echo "Nextcloud host found: $nextcloud_found"
+echo "Collabora host found: $collabora_found"
+
+[[ "$nextcloud_found" == "true" && "$collabora_found" == "true" ]] || exit 1
