6bcbea49093c5edabda73e56ee1bed6cb38f5452
Simon Beck <simon.beck@vshn.ch>
2025-05-28 11:43:25 +0000

    Make the script shellcheck proof

--- hack/getversionlist.sh
+++ hack/getversionlist.sh
@@ -9,9 +9,12 @@ version_list=${1}
 # Depending on the commodore compilation method it's not always the same.
 base_dir=${2}
 
+# shellcheck disable=SC2120
 function parse_yaml {
    local prefix=$2
+   # shellcheck disable=SC2155
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+    # shellcheck disable=SC2086,SC2120,SC1087
    sed -ne "s|^\($s\):|\1|" \
         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
@@ -31,6 +34,7 @@ cd "${base_dir}"
 revs=$(git rev-list --tags)
 
 # We get the last 20 component versions
+# shellcheck disable=SC2086
 git_list="$(git describe --abbrev=0 --tags --always $revs | grep  "v.*" | sort -uVr | head -n 20 )"
 
 rm -f "$version_list"
@@ -38,11 +42,14 @@ rm -f "$version_list"
 while read -r version rest
 do
   hash=$(git rev-list -n 1 "$version")
+  # shellcheck disable=SC2119
   eval "$(git show "$hash":class/defaults.yml | parse_yaml )"
+  # shellcheck disable=SC2154
   echo "${version}-${parameters_appcat_images_appcat_tag}" >> "$version_list"
 done <<< "$git_list"
 
 # Now we get the last 5 appcat version, as the component can change without appcat changes
+# shellcheck disable=SC2002
 filtered_versions=$(cat "$version_list" | sort -uVr | head -n 5)
 
 echo "$filtered_versions" > "$version_list"
